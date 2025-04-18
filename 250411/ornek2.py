24d1da268167f41d52708392ff4c5e4355738c74
hanan494 <mhmwdalzybq50@gmail.com>
2025-04-18 12:10:22 +0300

    Add files via upload

---
+++ 250411/ornek2.py
@@ -0,0 +1,9 @@
+kod=input("plakakodu yzınız:")
+if kod=="06":
+    print("ankara")
+elif kod=="07": 
+    print("antalya")
+elif kod=="08":
+    print("artvin")
+else:
+    print("türkiye")           
